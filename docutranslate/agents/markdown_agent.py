a67f9e2097f412521104cdf7e8bd8ea0329a5574
xunbu <xunbu3@qq.com>
2025-05-12 21:28:57 +0800

    优化提示词

--- docutranslate/agents/markdown_agent.py
+++ docutranslate/agents/markdown_agent.py
@@ -19,7 +19,7 @@ NO explanations. NO notes.
 不要修改标题的级别（如一级标题不要修改为二级标题）
 形如<ph-abc123>的占位符不要改变
 code、latex和HTML保持结构
-对于修复后的latex文本，用$包裹以构建合法表达式
+所有公式（包括短公式）都应该是合法的latex形式，包括要被$正确包裹等
 # 输出
 修正后的markdown纯文本（不是markdown代码块）
 # 示例
@@ -29,11 +29,11 @@ code、latex和HTML保持结构
 你叫
 输出：
 你叫什么名字
-## 去掉异常字词(保持占位符和latex符号)
+## 去掉异常字词与修正latex
 输入：
 一道\题@#目<ph-12asd2>:c_0+1=2，\(c 0\)等于几
 输出:
-一道题目<ph-12asd2>:：$c_0+1=2$，\(c_0\)等于几
+一道题目<ph-12asd2>:$c_0+1=2$，\(c_0\)等于几
 \no_think"""
 
 
@@ -50,9 +50,10 @@ You are a professional, authentic machine translation engine.
 If translation is unnecessary (e.g. proper nouns, codes, etc.), return the original text. 
 NO explanations. NO notes.
 不要修改标题的级别（如一级标题不要修改为二级标题）
-文献名保持原文
+文献名和文献作者名保持原文
 形如<ph-abc123>的占位符不要改变
 code、latex和HTML只翻译说明文字，其余保持原文
+公式必须表示为合法的latex公式
 # 输出
 翻译后的markdown纯文本（不是markdown代码块）
 # 示例
@@ -61,7 +62,12 @@ code、latex和HTML只翻译说明文字，其余保持原文
 hello<ph-aaaaaa>, what's your name?
 输出：
 你好<ph-aaaaaa>，你叫什么名字？
-## 文献名不翻译
+## 公式要为合法latex（包括需要被$包裹）
+输入：
+c_0+1=2
+输出：
+$c_0+1=2$
+## 不翻译文献名和文献作者名
 输入：
 [2] M. Castro, B. Liskov, et al. Practical byzantine fault tolerance. In OSDI,
 volume 99, pages 173–186, 1999.
