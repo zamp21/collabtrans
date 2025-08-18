3798f042be9437d98172b39d66a59e1f0fdfb59a
xunbu <xunbu3@qq.com>
2025-08-18 22:09:34 +0800

    small fix

--- docutranslate/global_values/__init__.py
+++ docutranslate/global_values/__init__.py
@@ -4,5 +4,5 @@ from .conditional_import import available_packages, conditional_import
 
 USE_PROXY = True if (os.getenv("DOCUTRANSLATE_PROXY_ENABLED") and os.getenv(
     "DOCUTRANSLATE_PROXY_ENABLED").lower() == "true") else False
-
-print(f"USE_PROXY:{USE_PROXY}")
+if USE_PROXY:
+    print(f"USE_PROXY:{USE_PROXY}")
