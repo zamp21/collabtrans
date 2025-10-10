847fd3487184e66a8a13c27bfb5b823b1943c0af
zampher <zamp21@163.com>
2025-10-10 22:12:55 +0800

    Recover docx extras

--- collabtrans/converter/x2md/__init__.py
+++ collabtrans/converter/x2md/__init__.py
@@ -1,2 +1,11 @@
-# SPDX-FileCopyrightText: 2025 QinHan
-# SPDX-License-Identifier: MPL-2.0
+"""
+x2md utilities package
+
+Keep this file minimal to avoid import side-effects while building/starting
+the app. Import submodules explicitly where needed, e.g.:
+
+    from collabtrans.converter.x2md.docx_extras import extract_headers_footers
+
+"""
+
+__all__: list[str] = []
